fbe73f75da991500d4fda6fb21d0736e99a896d8
AM0312 <ansh.midha0312@gmail.com>
2024-06-15 17:51:28 +0530

    app and utils added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
-
-// // Connect to MongoDB.
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB connected");
-//   } catch (error) {
-//     console.error("MongoDB connection error", error);
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
